7a3eefae6454a51e759f3c400903d506ab49d70f
katakori619 <kmdht9012@gmail.com>
2024-07-02 19:00:15 +0300

    amogos

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,31 @@ $('.clickable').on('click',function(){
         $(this).next().addClass('clicked')
     }
 })
+$('textarea').on('input',function(){
+    let x = $('textarea').val().length
+    if(x>100){
+        document.getElementById('charcounter').innerHTML = 'your available character finished'
+    }
+    else{
+        document.getElementById('charcounter').innerHTML = 100 - x
+    }
+})
+document.addEventListener('DOMContentLoaded',function(){
+    function updatetime(){
+        var dateFuture = new Date(2024 , 6 , 26);
+        var dateNow = new Date();
+        var seconds = Math.floor((dateFuture - (dateNow))/1000);
+        var minutes = Math.floor(seconds/60);
+        var hours = Math.floor(minutes/60);
+        var days = Math.floor(hours/24);
+        hours = hours-(days*24);
+        minutes = minutes-(days*24*60)-(hours*60);
+        seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+        $('.days').text(days + ' D')
+        $('.hours').text(hours + ' h')
+        $('.minutes').text(minutes + ' m')
+        $('.seconds').text(seconds + ' s')
+    }
+    updatetime()
+    setInterval(updatetime,1000)
+})
